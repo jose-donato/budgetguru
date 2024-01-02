ad83932c6b1172fbe994f82f355bd97e242c3668
jose-donato <43375532+jose-donato@users.noreply.github.com>
2024-01-02 15:59:27 +0000

    fix: adds shadcn ui and some meta

--- app/components/ui/drawer.tsx
+++ app/components/ui/drawer.tsx
@@ -41,12 +41,12 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border border-zinc-200 bg-white dark:border-zinc-800 dark:bg-zinc-950",
         className
       )}
       {...props}
     >
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
+      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-zinc-100 dark:bg-zinc-800" />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
@@ -96,7 +96,7 @@ const DrawerDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Description
     ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
+    className={cn("text-sm text-zinc-500 dark:text-zinc-400", className)}
     {...props}
   />
 ))
--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { SETTINGS } from "./lib/constants";
 import "./tailwind.css";
 
 import {
@@ -7,19 +8,29 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  MetaFunction
 } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: SETTINGS.title },
+    { name: "description", content: SETTINGS.description },
+  ];
+};
 
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💰</text></svg>" />
+        <meta name="color-scheme" content="dark" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-background text-foreground">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
